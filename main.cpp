4ff01992a761dd538c6e72516bc8e447cd05e3c5
eric7722 <eric7722@gmail.com>
2016-08-25 22:14:08 +0800

    測試列出檔案

--- main.cpp
+++ main.cpp
@@ -41,7 +41,34 @@ void ListDir()
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+    WIN32_FIND_DATA _fdata;
+    HANDLE hFind = INVALID_HANDLE_VALUE;
+    LARGE_INTEGER filesize;
+
     hInst=hInstance;
     InitCommonControls();
+
+    hFind = FindFirstFile("D:\\GitHub\\KKJ",&_fdata);
+    if (INVALID_HANDLE_VALUE == hFind)
+    {
+     //   DisplayErrorBox(TEXT("FindFirstFile"));
+        //return dwError;
+    }
+
+    do
+   {
+      if (_fdata.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+      {
+         //_tprintf(TEXT("  %s   <DIR>\n"), ffd.cFileName);
+         MessageBox(NULL,_fdata.cFileName,"NAME",MB_ICONWARNING | MB_DEFBUTTON2);
+      }
+      else
+      {
+         MessageBox(NULL,_fdata.cFileName,"NAME",MB_ICONWARNING | MB_DEFBUTTON2);
+      }
+   }
+   while (FindNextFile(hFind, &_fdata) != 0);
+
+
     return DialogBox(hInst, MAKEINTRESOURCE(DLG_MAIN), NULL, (DLGPROC)DlgMain);
 }
